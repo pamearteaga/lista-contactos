d86237f765d3204015cace9ee75a2e6d05da1b44
pamearteaga <pe_arteaga@hotmail.com>
2017-12-11 20:31:55 -0300

    Lista de Contactos

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+//var id = 0;
+
+$(document).ready(() => {
+
+});
+
+$("#save").click(() => {
+
+    function addContact(){
+    var nombre = $("#contactName").val();
+    var numero = $("#contactNumber").val();
+
+    $("#contactos").append('<div class="col-lg-6">' + 
+        '<div class="panel panel-default">' +
+        '<div class="panel-body">' + '<h2>' + nombre + '</h2>' 
+        + '<hr>' + '<p>' + numero + '</p>'
+        + '</div>' + '</div>' + '</div>');
+    };
+ });
+
+
+    //Este es el ID de este pin en particular
+    /* var pinId = "#pin_" + idPin;
+    $("#pin_" + idPin).mouseover(() => {
+        $(pinId + " > .front").fadeTo(1000, 0, () => {
+            $(pinId + " > .front").hide();
+            $(pinId + " > .rear").fadeTo(1000, 1);
+        });
+    });*/
+
+    //id++;
